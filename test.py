5bca30e03001f044bf177cb36c041dc58cd0581f
Negar Mirgati <negar.nbj96@yahoo.com>
2018-07-17 12:42:53 +0430

    Implement Bestfit

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import random
 import sqlite3
 
-MAX_CPU = 100
+MAX_CPU = 500
 MAX_MEM = 2048
-MAX_DCS = 20
-MAX_NODES = 10
-MAX_REQS = 200
+MAX_DCS = 30
+MAX_NODES = 20
+MAX_REQS = 40
 
 class Queue:
 
@@ -22,7 +22,7 @@ class Queue:
   def dequeue(self):
       if len(self.queue)>0:
           return self.queue.pop()
-      return ("Queue Empty!")
+      return (-1,-1)
 
   def size(self):
       return len(self.queue)
@@ -106,15 +106,92 @@ class Database:
     for row in self.c.execute("SELECT * FROM Node"):
      print(row)
 
+  def getPossibleDCs(self, cpu, mem):
+    return (self.c.execute("SELECT DCID FROM DataCenter WHERE CPU >= " + str(cpu) + " AND MEM >= " + str(mem)))
 
+  def getPossibleNodes(self, possible_dcs, cpu, mem):
+    dcid_list = []
+    str1 = "("
+    flag = False
+    for row in possible_dcs:
+      #print(" row is ", row)
+      if(flag == False) :
+        str1 += str(row[0])
+        flag = True
+      else:
+        str1 += " ," + str(row[0]) 
+      
+    str1 += ")"
+    #print(str1)
+
+    return (self.c.execute("SELECT * FROM Node WHERE FDCID IN " + str1 + " AND CPU >= " + str(cpu) + " AND MEM >= " + str(mem)))
+
+
+  def findBestNode(self, possible_nodes, cpu, mem):
+    min_node = 0
+    min_dc = 0
+    min_cpu = float("inf")
+    min_mem = float("inf")
+
+    for row in possible_nodes:
+      cpu_diff = row[2] - min_cpu
+      mem_diff = row[3] - min_mem
+      if(cpu_diff <= 0 and mem_diff <= 0):
+        # TODO
+        min_node = row[0]
+        min_dc = row[1]
+
+    print(min_node, min_dc)
+    return(min_node, min_dc)
+
+  def updateDCTable(self, dcid, cpu, mem):
+    print("updating dctable dc with id  " + str(dcid))
+    for row in (self.c.execute("SELECT * FROM DataCenter WHERE DCID = " + str(dcid))):
+      print(row)
+    self.c.execute("UPDATE DataCenter SET CPU = CPU - " + str(cpu) + ", MEM = MEM - " + str(mem) + " WHERE DCID = " + str(dcid))
+    for row in self.c.execute("SELECT * FROM DataCenter WHERE DCID = " + str(dcid)):
+      print(row)
+
+  def updateNodeTable(self, node, cpu, mem):
+
+    print("updating nodetable with node id  " + str(node))
+    for row in (self.c.execute("SELECT * FROM Node WHERE NodeID= " + str(node))):
+      print(row)
+    self.c.execute("UPDATE Node SET CPU = CPU - " + str(cpu) + ", MEM = MEM - " + str(mem) + " WHERE NodeID = " + str(node))
+    for row in (self.c.execute("SELECT * FROM Node WHERE NodeID= " + str(node))):
+      print(row)
+
+
+def distributeTasks(myQueue, myDB):
+
+  cntr = 1
+  #myDB.printNodeTable()
+  while(True):
+    request = myQueue.dequeue()
+    if(request != (-1, -1)):
+      cpu = request[0]
+      mem = request[1]
+      print("Task No." + str(cntr) + " needs cpu = " + str(cpu) + " and mem = " + str(mem))
+      possible_dcs = myDB.getPossibleDCs(cpu, mem)
+      possible_nodes = myDB.getPossibleNodes(possible_dcs, cpu, mem)
+      (BestNode, BestDC) = (myDB.findBestNode(possible_nodes, cpu, mem))
+      if(BestNode == 0 or BestDC == 0): 
+        print("Task no." + str(cntr) +  " could not be assigned to any node.")
+      else:
+        myDB.updateDCTable( BestDC, cpu, mem)
+        myDB.updateNodeTable( BestNode, cpu, mem)
+      cntr += 1
+    else:
+      print("Finished distrubuting Tasks")
+      return
 
 
 def main():
 
   myQueue = Queue()
   initiateQueue(myQueue)
-  size = myQueue.size()
-  print(size)
+
+  #distributeTasks(myQueue)
 
   myDB = Database()
   myDB.createDataCenterTable()
@@ -122,10 +199,14 @@ def main():
   
   num_of_dcs = myDB.generateRandomDCs()
   myDB.generateRandomNodes(num_of_dcs)
-  myDB.printDCTable()
-  myDB.printNodeTable()
 
-  print("here")
+  #myDB.printNodeTable()
+
+
+  distributeTasks(myQueue, myDB)
+  #myDB.printDCTable()
+  #myDB.printNodeTable()
+
 
 if __name__== "__main__":
   main()
